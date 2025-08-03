8cc4fddbc58015e271a078e028b97504877a52de
sgilb <32495429+sgilb@users.noreply.github.com>
2025-08-03 20:29:45 +0100

    Support local image files in HTML template

--- webpack.common.js
+++ webpack.common.js
@@ -18,7 +18,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      }
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
   },
 };
